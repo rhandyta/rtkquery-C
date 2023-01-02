8c15bcc057776ad5bd68189fe226b7bb16dc5207
Rhandyta Briantama <rhandytabriantama@gmail.com>
2023-01-02 12:43:53 +0700

    api slice

---
+++ src/app/features/apiSlice.js
@@ -0,0 +1,7 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const apiSlice = createApi({
+    baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3005/" }),
+});
+
+export default apiSlice;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
+import { ApiProvider } from "@reduxjs/toolkit/dist/query/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import apiSlice from "./app/features/apiSlice";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-        <App />
+        <ApiProvider api={apiSlice}>
+            <App />
+        </ApiProvider>
     </React.StrictMode>
 );
